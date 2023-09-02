1d943e02005b2fbea7ce303511f29bdc0bc735dd
bluesteelwater <105950561+bluesteelwater@users.noreply.github.com>
2023-09-01 22:50:03 -0500

    Add files via upload

---
+++ p110.py
@@ -0,0 +1,59 @@
+import random
+
+
+input(int('Would you like to roll a dice? n for no and y for yes '))
+if input == 'y':
+    no = random.randint(1,6)
+
+if no == 1:
+    print("[------]")
+    print("[      ]")
+    print("[  0   ]")
+    print("[      ]")
+    print("[------]")
+else:
+    if no == 2:
+        print("[------]")
+        print("[      ]")
+        print("[  0   ]")
+        print("[      ]")
+        print("[------]")
+    else:
+            if no == 3:
+                print("[------]")
+                print("[  0   ]")
+                print("[      ]")
+                print("[ 0  0 ]")
+                print("[------]")
+            else:
+                if no == 4:
+                    print("[------]")
+                    print("[ 0  0 ]")
+                    print("[      ]")
+                    print("[ 0  0 ]")
+                    print("[------]")
+                else:
+                    if no == 5:
+                        print("[------]")
+                        print("[ 0  0 ]")
+                        print("[  0   ]")
+                        print("[ 0  0 ]")
+                        print("[------]")
+                    else:
+                        if no == 6:
+                            print('[------]')
+                            print('[ 0 0  ]')
+                            print('[ 0 0  ]')
+                            print('[ 0 0  ]')
+                            print('[------]')
+
+    if input == 'n':
+        print('okay, goodbye! :( ')
+
+
+
+                        
+                            
+                    
+            
+            
